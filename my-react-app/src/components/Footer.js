23b26cb4a9ab00493a08e62fc4b0ae1f24b5dfe6
Wilal668 <150358327+Wilal668@users.noreply.github.com>
2024-05-13 01:13:51 +0200

    service page upgrade

--- my-react-app/src/components/Footer.js
+++ my-react-app/src/components/Footer.js
@@ -2,11 +2,11 @@
 function Footer(){
     return(
         <footer>
-        <div class="copyright">Copyright © 2023 My Website</div>
+        <div class="copyright">Copyright © 2024 Strona Wędkarska</div>
         <div class="social-links">
-            <a href="#">Facebook</a>
-            <a href="#">Twitter</a>
-            <a href="#">Instagram</a>
+            <a href="https://www.facebook.com/share/1ugUTYMQYvg5C1tj/">Facebook</a>
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">Youtube</a>
+            <a href="https://www.instagram.com/large_wojtek_dobre?igsh=ZWd3MWhrM2o1MTBu">Instagram</a>
         </div>
         </footer>
     );
--- my-react-app/src/components/pages/ServicesPage.js
+++ my-react-app/src/components/pages/ServicesPage.js
@@ -1,10 +1,104 @@
 import React from 'react';
+import { useState } from 'react';
+
+import OleśnikowaDolinaPhoto from './img/oleśnikowa-dolina.png';
+import EbroPhoto from './img/ebro.png';
+import EkoFarmaPhoto from './img/ekofarma.png';
+import WedkaPhoto from './img/gear-wedka.jpg';
+import KolowrotekPhoto from './img/gear-kolowrotek.jpg';
+import PrzynetyPhoto from './img/gear-przynety.jpg';
 
 function ServicesPage() {
+  const [fishingSpots, setFishingSpots] = useState([
+    {
+      name: 'Oleśnikowa Dolina',
+      location: 'Polska, Oleśnik 1, 05-620',
+      photo: OleśnikowaDolinaPhoto,
+    },
+    {
+      name: 'Rzeka Ebro',
+      location: '50691, Saragossa, Hiszpania',
+      photo: EbroPhoto,
+    },
+    {
+      name: 'EkoFarma',
+      location: 'Polska, Pałacowa 58, 96-321 Grzegorzewice',
+      photo: EkoFarmaPhoto,
+    },
+  ]);
+
+  const [selectedSpot, setSelectedSpot] = useState(null);
+
+  const handleSpotSelect = (spot) => {
+    setSelectedSpot(spot);
+  };
+
+  const [fishingGear, setFishingGear] = useState([
+    { name: 'Wędka', price: 50, photo: WedkaPhoto },
+    { name: 'Kołowrotek', price: 30, photo: KolowrotekPhoto },
+    { name: 'Przynęty', price: 20, photo: PrzynetyPhoto},
+  ]);
+
+
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (gear) => {
+    setCart([...cart, gear]);
+  };
+
+  const handleClearCart = () => {
+    setCart([]);
+  };
+
+  const calculateCartValue = () => {
+    return cart.reduce((total, gear) => {
+      return total + gear.price;
+    }, 0);
+  };
+
   return (
-    <div className="services-page">
-      <h2>Our Services</h2>
-      <p>This is the services page.</p>
+    <div class="services-page">
+      <h1>Services:</h1>
+      <h2>Fishing Spots</h2>
+      <ul>
+        {fishingSpots.map((spot) => (
+          <li key={spot.name}>
+            <img src={spot.photo} alt={spot.name} />
+            <a href="#" onClick={() => handleSpotSelect(spot)}>
+              {spot.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+      {selectedSpot && (
+        <div class="selected-spot">
+          <h3>Selected Spot: {selectedSpot.name}</h3>
+          <img src={selectedSpot.photo} alt={selectedSpot.name} width="200px" />
+          <p>Location: {selectedSpot.location}</p>
+        </div>
+      )}
+      <h2>Fishing Gear</h2>
+      <ul>
+        {fishingGear.map((gear) => (
+          <li key={gear.name}>
+            <img src={gear.photo} alt={gear.name}/>
+            <a href="#" onClick={() => handleAddToCart(gear)}>
+              {gear.name} (${gear.price})
+            </a>
+          </li>
+        ))}
+      </ul>
+      <h2>Cart</h2>
+      <ul class="cart">
+        {cart.map((gear) => (
+          <li key={gear.name}>
+            <img src={gear.photo} alt={gear.name}/>
+            {gear.name} (${gear.price})
+          </li>
+        ))}
+      </ul>
+      <p> Cart value: (${calculateCartValue()})</p>
+      <button class="cart-button" onClick={handleClearCart}>Clear Cart</button>
     </div>
   );
 }
